572ebb004f21ae99c3c59c550a8351801f26d13a
RajKotadia <rajkotadia15@gmail.com>
2020-06-25 00:06:45 +0530

    :tada: Initial project setup

---
+++ django_auth/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from users import views as user_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', user_views.home, name='home'),
+]
